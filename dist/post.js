e016b67d9e1887da783145e808ea82e6f1df3970
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2024-09-20 15:53:36 +0200

    Dist

--- dist/post.js
+++ dist/post.js
@@ -23069,26 +23069,44 @@ var require_common = __commonJS({
           const token = core.getInput("token");
           const ref = core.getInput("ref");
           const except = core.getInput("except");
+          const include = core.getInput("includePrefix");
           const keep = parseInt(core.getInput("keep"), 10);
           const octokit = github.getOctokit(token);
-          const result = await octokit.rest.actions.getActionsCacheList({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            ref
-          });
-          console.log(JSON.stringify(result.data));
-          const caches = result.data.actions_caches;
-          console.log(`Cache keys for ${ref}: ${caches.length}, keep ${keep}`);
-          const cachesToDelete = caches.slice(keep);
-          for (const cache of cachesToDelete) {
-            if (cache.key !== except) {
-              console.log(`Deleting cache: ${cache.key}`);
-              await octokit.rest.actions.deleteActionsCacheById({
+          console.log(`ref: ${github.context.ref}`);
+          const branches = [];
+          if (ref === "") {
+            if (github.context.payload.pull_request) {
+              const { data: prData } = await octokit.rest.pulls.get({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
-                cache_id: cache.id
+                pull_number: github.context.payload.pull_request.number
               });
+              branches.push(`refs/heads/${prData.head.ref}`, `refs/pull/${github.context.payload.pull_request.number}/merge`);
+            } else {
+              branches.push(github.context.ref);
             }
+          } else {
+            branches.push(ref);
+          }
+          let cachesToDelete = [];
+          for (const ref2 of branches) {
+            const result = await octokit.rest.actions.getActionsCacheList({
+              owner: github.context.repo.owner,
+              repo: github.context.repo.repo,
+              ref: ref2
+            });
+            const refCaches = result.data.actions_caches;
+            console.log(`Cache keys for ${ref2}: ${refCaches.length}`);
+            cachesToDelete.push(...refCaches);
+          }
+          cachesToDelete = cachesToDelete.sort((a, b) => new Date(b.last_accessed_at) - new Date(a.last_accessed_at)).filter((cache) => cache.key !== except && cache.key.startsWith(include)).slice(keep);
+          for (const cache of cachesToDelete) {
+            console.log(`Deleting cache: ${cache.key}`);
+            await octokit.rest.actions.deleteActionsCacheById({
+              owner: github.context.repo.owner,
+              repo: github.context.repo.repo,
+              cache_id: cache.id
+            });
           }
         } catch (error2) {
           core.setFailed(error2.message);
